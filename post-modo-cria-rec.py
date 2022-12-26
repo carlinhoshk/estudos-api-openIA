c7fb365df3ba1ed195b7fbe2c2454773316dc0f8
carlinhoshk <carlosmdohk@gmail.com>
2022-12-26 00:18:31 -0300

    testando modelo davinci003

--- post-modo-cria-rec.py
+++ post-modo-cria-rec.py
@@ -3,7 +3,7 @@ import openai
 openai.api_key = os.getenv("TOKEN_GPT")
 retorno = openai.Completion.create(
   model="text-davinci-003",
-  prompt="Fale 'isso é um teste' com dia e horario de agora.",
+  prompt="Calcule quanto é 3x10 e Fale 'isso é um teste' .",
   max_tokens=22,
   temperature=0
 )
